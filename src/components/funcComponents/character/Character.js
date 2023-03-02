65611d21530bd87ecac82ac83ddcc45dd1c13715
Donald961 <donald.titoldini@gmail.com>
2023-03-02 10:42:04 +0100

    LastBranch

--- src/components/funcComponents/character/Character.js
+++ src/components/funcComponents/character/Character.js
@@ -4,18 +4,8 @@ import Spritesheet from "react-responsive-spritesheet";
 import "./character.css";
 import bat from "../../../assets/images/bat.png";
 
-
-//import { React, useRef, useEffect, forwardRef } from "react";
-//import PropTypes from "prop-types";
-//import Spritesheet from "react-responsive-spritesheet";
-//import "./character.css";
-
 const Character = forwardRef((props, ref) => {
   let charRef = useRef(null);
-
-  function callback(e) {
-    props.callbackInput(e);
-  }
 
   useEffect(() => {
     if (ref) {
@@ -38,32 +28,12 @@ const Character = forwardRef((props, ref) => {
   return (
     <div
       className={"divRed"}
-      onClick={callback}
       ref={charRef}
       style={{
-        transform: `translateY(${props.distanceTop}vh)`,
+        transform: translateY(${props.distanceTop}vh),
         transition: "0.3s ease-in",
       }}
     >
-            <Spritesheet
-        className={"divCharacter"}
-        image={bat}
-        widthFrame={500}
-        heightFrame={500}
-        steps={6}
-        fps={8}
-        autoplay={true}
-        loop={true}
-        timeout={true}
-      />
-
-    </div>
-  );
-});
-
-  /*
-  return (
-    <div className="divRed">
       <Spritesheet
         className={"divCharacter"}
         image={bat}
@@ -77,7 +47,7 @@ const Character = forwardRef((props, ref) => {
       />
     </div>
   );
-}*/
+});
 
 export default Character;
 
--- src/screens/Skin.js
+++ src/screens/Skin.js
@@ -1,15 +1,65 @@
 import React from "react";
+import { useState } from "react";
+import {Howl, Howler} from 'howler';
+import Input from "../components/ui/input/Input";
 import Spritesheet from "react-responsive-spritesheet";
+import GamePhrase from "../components/ui/gamePhrase/GamePhrase";
+import ParallaxBG from "../components/ui/parallaxBG/ParallaxBG";
 import bat from "../assets/images/bat.png";
+import batBlue from "../assets/images/batBlue.png";
+import batRed from "../assets/images/batRed.png";
+import clickSkin from "../assets/sounds/clickSkin.flac";
+import click from "../assets/sounds/click.wav";
 import "../styles/skin/skin.css";
 
-function Skin() {
+function Skin(props) {
   
+  const [state, setState] = useState (
+    {
+    skinMod: bat,
+    skinBlock2: true,
+    skinBlock3: true,
+    }
+  );
+  const {Howl, Howler} = require('howler');
+
+  let points: 500;
+  let clickChange = new Howl({
+    src: [clickSkin]
+  });
+  let soundClick = new Howl({
+    src: [click]
+  });
+
+  function changeSkin(e) {
+    clickChange.play();
+    let skin = e.target.value;
+    let skinChange = null;
+
+    if (skin === "SkinOne") {
+      skinChange = bat;
+    } else if (skin === "SkinTwo") {
+      skinChange = batBlue;
+    } else {
+      skinChange = batRed;
+    }
+
+    setState({
+      skinMod: skinChange,
+    })
+
+  }
+
+  function applySkin() {
+    soundClick.play();
+  }
+
   return (
-    <div className="divRed">
+    <div className="skinMain">
+      <ParallaxBG/>
       <Spritesheet
-        className={"divCharacter"}
-        image={bat}
+        className={"charPos"}
+        image={state.skinMod}
         widthFrame={500}
         heightFrame={500}
         steps={6}
@@ -18,8 +68,55 @@ function Skin() {
         loop={true}
         timeout={true}
       />
+      <div className="skinInput">
+        <div className="skinRowTitle">
+            <GamePhrase
+              classNameGamePhrase={"titleSkin"}
+              h1GamePhrase={"Skin 1"}
+            />
+            <GamePhrase
+              classNameGamePhrase={"titleSkin"}
+              h1GamePhrase={"Skin 2"}
+            />
+            <GamePhrase
+              classNameGamePhrase={"titleSkin"}
+              h1GamePhrase={"Skin 3"}
+            />
+        </div>
+        <div className="skinRowRadio">
+          <Input
+            cssStyleInput={""}
+            nameInput={"skin"}
+            valueInput={"SkinOne"}
+            typeInput={"radio"}
+            callbackInput={changeSkin}
+          />
+          <Input
+            cssStyleInput={""}
+            nameInput={"skin"}
+            valueInput={"SkinTwo"}
+            typeInput={"radio"}
+            callbackInput={changeSkin}
+          />
+          <Input
+            cssStyleInput={""}
+            nameInput={"skin"}
+            valueInput={"SkinThree"}
+            typeInput={"radio"}
+            callbackInput={changeSkin}
+          />
+        </div>
+        <div className="skinColButton">
+          <Input
+              cssStyleInput={"defaultButton"}
+              valueInput={"Apply Skin"}
+              typeInput={"button"}
+              onClickInput={applySkin}
+            />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Skin;
